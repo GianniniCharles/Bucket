29edb1f9277ddaab63407de954b07f665702ec5a
Giannini Charles <12gcharles@gmail.com>
2020-05-25 13:24:48 -0400

    bad approach 2

--- swiftInDepth/ProtocolAssociatedType.playground/Contents.swift
+++ swiftInDepth/ProtocolAssociatedType.playground/Contents.swift
@@ -4,22 +4,30 @@ var str = "Hello, playground"
 
 
 protocol Worker {
-    
     @discardableResult
-    func start(input: String) -> Bool
-    
+    func start(input: Input) -> Output
 }
+
+protocol Input {}
+protocol Output {}
 
 
 class MailJob: Worker {
-    func start(input: String) -> Bool {
+    func start(input: Input) -> Output {
         //send mail to emailAddress. input can be email
         // return if successed.
-        return true
+        let output: Output
+        return output
     }
 }
 
 class FileRemover: Worker {
+    
+    func start(input: Input) -> Output {
+        let output: Output
+        return output
+        //bad, not scalable.
+    }
     // input is url
     // outpul is [string]
 }
